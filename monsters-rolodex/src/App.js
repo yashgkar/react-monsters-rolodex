7557165cae4cb61fc4ba94c14380c433e8f76ee8
Yash Garudkar <yashgarudkar@gmail.com>
2020-04-18 23:59:04 +0530

    1st push

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.components';
+import './App.css';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input
+          type='search'
+          placeholder='Search Monsters'
+          onChange={e => {
+            this.setState({searchField: e.target.value});
+          }}
+        />
+        <CardList monsters={this.state.monsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
