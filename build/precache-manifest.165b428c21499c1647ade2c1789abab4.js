1e79b79165c6cf2390d1b17098591aee316a97e5
Yash Garudkar <yashgarudkar@gmail.com>
2020-04-22 21:41:09 +0530

    adding files for github pages

---
+++ build/precache-manifest.165b428c21499c1647ade2c1789abab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a30b7d429ddcc38a252c775a47bad38a",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "36a926cad31e5918cb80",
+    "url": "/react-monsters-rolodex/static/css/main.7a9d665b.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/react-monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36a926cad31e5918cb80",
+    "url": "/react-monsters-rolodex/static/js/main.2c27406b.chunk.js"
+  },
+  {
+    "revision": "b2cb901566663f18323e",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.a30671e1.js"
+  }
+]);
